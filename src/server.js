0f1f4039be7e6ca81ebf4d3767edaa118bd734e3
Great Ezenna <ezennagreat@gmail.com>
2025-09-13 13:38:18 +0100

    backbone code for server, parsing the http request message

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-import net from "node:net"
+import net from "node:net";
+
+const server = net.createServer((socket) => {
+  let requestData = "";
+
+  socket.on("data", (data) => {
+    requestData += data;
+
+    const [head, body] = requestData.split("\r\n\r\n")
+
+    console.log({ head, body })
+
+  });
+});
+
+server.listen(8888);
