64ca0cd6200460a136f7bd59e8db4dee8cde31b8
Great Ezenna <ezennagreat@gmail.com>
2025-09-13 19:58:32 +0100

    refactored route method to add to the this.routes in memory storage

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import net from "node:net";
 import { EventEmitter } from "node:events";
 
-
 class Server extends EventEmitter {
   #_server;
 
@@ -9,29 +8,31 @@ class Server extends EventEmitter {
     super();
     this.port = port;
     this.host = host;
-    this.routes = ["GET", "POST"]
+    this.routes = new Map();
   }
 
-  #handleRequestMessage(chunk) {
-    
-  }
+  #handleRequestMessage(chunk) {}
 
   start() {
     this.#_server = net.createServer((_socket) => {
-        _socket.on("data", this.#handleRequestMessage)
+      _socket.on("data", (chunk) => {
+        this.#handleRequestMessage(chunk);
+      });
     });
 
-    this.#_server.listen(this.port, this.host)
+    this.#_server.listen(this.port, this.host);
   }
-  
+
   route(method, path, routeHandler) {
-    if (!this.routes.includes(method.toUpperCase())) {
-        let err = new TypeError(`${method} method is not supported!`)
-        routeHandler(err)
+    if (!["GET", "POST"].includes(method.toUpperCase())) {
+      throw new TypeError(`${method} method is not supported!`); 
     }
+
+    const route = `${method.toUpperCase()}:${path}`;
+    this.routes.set(route, routeHandler)
   }
 }
 
-const miniServer = { Server }
+const miniServer = { Server };
 
-export default miniServer 
+export default miniServer;
