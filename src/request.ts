cd20e0eafdf12e96749e0f8af3d6b19c8193cad7
Great Ezenna <ezennagreat@gmail.com>
2025-09-24 16:03:38 +0100

    implemented the getHeader method

--- src/request.ts
+++ src/request.ts
@@ -1,23 +1,36 @@
 import net from "node:net";
 import { Readable } from "node:stream";
-import utils from "./utils/httpParser";
 
 interface ParsedRequestMessage {
-    method?: string | undefined,
-    path?: string | undefined,
-    version?: string | undefined,
-    headers?: Record<string, string> | undefined,
-    body?: string | undefined,
+    method?: string | undefined;
+    path?: string | undefined;
+    version?: string | undefined;
+    headers?: Record<string, string> | undefined;
+    body?: string | undefined;
 }
 
-type RequestMessageType = ParsedRequestMessage | undefined
+type RequestMessageType = ParsedRequestMessage | undefined;
 
 class RequestMessage extends Readable {
     requestMessage: RequestMessageType;
+    method: string;
+    path: string;
+    headers: Record<string, string>;
+    body: string;
     constructor(connection: net.Socket, parsedMessage: RequestMessageType) {
         super();
-        this.requestMessage = parsedMessage
+        this.requestMessage = parsedMessage;
+        this.method = parsedMessage?.method || "";
+        this.path = parsedMessage?.path || "";
+        this.headers = parsedMessage?.headers || {};
+        this.body = parsedMessage?.body || "";
+    }
+
+    getHeader(header: string): string | undefined {
+        return this.headers.hasOwnProperty(header)
+            ? this.headers[header]
+            : undefined;
     }
 }
 
-export default RequestMessage
+export default RequestMessage;
