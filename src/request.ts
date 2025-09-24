2288024fc9354b387992e0ed03e8ba25037d97d8
Great Ezenna <ezennagreat@gmail.com>
2025-09-24 15:00:58 +0100

    building up the request object

--- src/request.ts
+++ src/request.ts
@@ -1,7 +1,23 @@
+import net from "node:net";
 import { Readable } from "node:stream";
+import utils from "./utils/httpParser";
+
+interface ParsedRequestMessage {
+    method?: string | undefined,
+    path?: string | undefined,
+    version?: string | undefined,
+    headers?: Record<string, string> | undefined,
+    body?: string | undefined,
+}
+
+type RequestMessageType = ParsedRequestMessage | undefined
 
 class RequestMessage extends Readable {
-
+    requestMessage: RequestMessageType;
+    constructor(connection: net.Socket, parsedMessage: RequestMessageType) {
+        super();
+        this.requestMessage = parsedMessage
+    }
 }
 
 export default RequestMessage
