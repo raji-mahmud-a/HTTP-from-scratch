17d508627f566613abc901d72624b1a2c7540665
Great Ezenna <ezennagreat@gmail.com>
2025-09-26 11:33:20 +0100

    refactor ResponseMessage class to improve response building and status text retrieval

--- src/response.ts
+++ src/response.ts
@@ -1,21 +1,81 @@
-import net from "node:net"
+import net from "node:net";
 import { Writable } from "node:stream";
 
 class ResponseMessage extends Writable {
-    statusCode: number
-    headers: Record<string, string>
+    statusCode: number;
+    headers: Record<string, string>;
+    headersSent: boolean;
+    connection: net.Socket;
     constructor(connection: net.Socket) {
         super();
-        this.headers = {}
+        this.headers = {};
+        this.statusCode = 200;
+        this.headersSent = false;
+        this.connection = connection;
     }
 
     setHeader(key: string, value: string) {
         this.headers[key] = value;
     }
 
-    send(data: string) {
+    getStatusText(statusCode = this.statusCode) {
+        const statusTexts = {
+            200: "OK",
+            201: "Created",
+            204: "No Content",
+            301: "Moved Permanetly",
+            302: "Found",
+            304: "Not Modified",
+            400: "Bad Request",
+            401: "Unauthorized",
+            403: "Forbidden",
+            404: "Not Found",
+            405: "Method Not Allowed",
+            409: "Conflict",
+            422: "Unprocessable Entity",
+            500: "Internal Server Error",
+            502: "Bad Gateway",
+            503: "Service Unavailable",
+        };
 
+        return statusTexts.hasOwnProperty(statusCode)
+            ? statusTexts[statusCode]
+            : undefined;
+    }
+
+    buildResponse(
+        statusCode = this.statusCode,
+        headers = this.headers,
+        data: string
+    ) {
+        let responseMessage = "";
+        let statusText: string = this.getStatusText(statusCode) || "Unknown";
+
+        responseMessage += "HTTP/1.1" + " " + statusCode + statusText + "\r\n";
+
+        for (let header of Object.entries(headers)) {
+            const [key, value] = header;
+            responseMessage += key + ": " + value + "\r\n";
+        }
+
+        if (!headers.hasOwnProperty("Content-Length")) {
+            responseMessage += "Content-Length: " + Buffer.byteLength(data).toString() + "\r\n"
+        }
+
+        responseMessage += "\r\n" + data
+
+        return responseMessage;
+    }
+
+    _write(
+        chunk: any,
+        encoding: BufferEncoding,
+        callback: (error?: Error | null) => void
+    ): void {
+        // if (!this.headersSent) {
+        //     this.connection.write();
+        // }
     }
 }
 
-export default ResponseMessage
+export default ResponseMessage;
