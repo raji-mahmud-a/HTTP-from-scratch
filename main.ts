a90d5909d2e13604efc15f9288d229de2f4daf90
Great Ezenna <ezennagreat@gmail.com>
2025-09-26 11:46:27 +0100

    fix: ensure proper response handling in server request callback

--- main.ts
+++ main.ts
@@ -3,7 +3,10 @@ import HTTPServer from "./src/HttpServer";
 const server = new HTTPServer()
 
 server.makeServer((req, res) => {
-
+    res.setHeader("Content-Type", "text/html")
+    res.write("<h1>Hello</h1>")
+    res.connection.end()
 })
+ 
 
-server.on("request")
+server.listen()
